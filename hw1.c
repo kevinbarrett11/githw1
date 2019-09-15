398e69835add93ded31c70d5c4173f7554991b99
kevinbarrett11 <kbarre8@uic.edu>
2019-09-15 13:09:34 -0500

    finished_2

--- hw1.c
+++ hw1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "hw1.h"
 //Kbarre8 UIN: 675281207 9-15-19
-static void I_have_written(){}
+
+static int I_have_written(){
+return;}
+
 static int the_code[3] = {1,2,3};
 
 static int that_you_need(){static int to_compile =4; //have to_compile return to make sure it returns before you_need
@@ -14,6 +17,8 @@ int sides_and(){
     static unsigned int symbols[2]; //putting these variables in here would appear before sides
     static unsigned int ridiculous = 1;
     static unsigned int has_a_bunch_of = 44;
+   
+    return;
 }
 
 
@@ -21,6 +26,7 @@ int main(int argc,char* argv[]){
     char *id = "Kbarre8";  //put into char pointer so that printf would appear instead of puts
     printf(id);
     return 0;
+
 }
 void Forgive_me(){
 }
